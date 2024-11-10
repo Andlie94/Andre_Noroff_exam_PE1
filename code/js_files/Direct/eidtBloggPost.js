95554a5fbdbc39715c5a66206101e2bbd152074b
Andlie94 <160486912+Andlie94@users.noreply.github.com>
2024-11-10 21:55:57 +0100

    Add files via upload

--- code/js_files/Direct/eidtBloggPost.js
+++ code/js_files/Direct/eidtBloggPost.js
@@ -5,65 +5,75 @@ if (!token) {
 
 const urlParams = new URLSearchParams(window.location.search);
 const postId = urlParams.get("id");
-console.log(postId);
 
 async function fetchPostData() {
   try {
-    const response = await fetch(`https://v2.api.noroff.dev/blog/posts/andre_lier/${postId}`, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
+    const response = await fetch(
+      `https://v2.api.noroff.dev/blog/posts/andre_lier/${postId}`,
+      {
+        headers: {
+          Authorization: "Bearer ${token}",
+        },
+      }
+    );
 
     if (!response.ok) {
       throw new Error("Failed to fetch post data");
     }
 
     const post = await response.json();
-    document.getElementById("titleBloggEdit").value = post.title;
-    document.getElementById("editBloggContent").value = post.body;
-    document.getElementById("urlPictureEdit").value = post.media;
+
+    document.getElementById("titleBloggEdit").value = post.data.title;
+    document.getElementById("editBloggContent").value = post.data.body;
+    document.getElementById("urlPictureUrlEdit").value = post.data.media.url;
+    document.getElementById("urlPictureAltEdit").value = post.data.media.alt;
   } catch (error) {
-    console.error("Error fetching post data:", error);
+    console.error('Error fetching post data:', error);
   }
 }
 
-document.getElementById("editPost").addEventListener("submit", async (event) => { 
-  event.preventDefault();
+document
+  .getElementById('editPost')
+  .addEventListener('submit', async (event) => {
+    event.preventDefault();
 
-  const title = document.getElementById("titleBloggEdit").value;
-  const body = document.getElementById("editBloggContent").value; 
-  const media = document.getElementById("urlPictureEdit").value; 
+    const title = document.getElementById("titleBloggEdit").value;
+    const body = document.getElementById("editBloggContent").value;
+    const mediaUrl = document.getElementById("urlPictureUrlEdit").value;
+    const mediaAlt = document.getElementById("urlPictureAltEdit").value;
 
-  const updatedContent = {
-    title: title,
-    body: body,
-    media: {
-        url: media
-    }
-  };
-
-  try {
-    const response = await fetch(`https://v2.api.noroff.dev/blog/posts/andre_lier/${postId}`, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`,
+    const updatedContent = {
+      title: title,
+      body: body,
+      media: {
+        url: mediaUrl,
+        alt: mediaAlt,
       },
-      body: JSON.stringify(updatedContent),
-    });
+    };
 
-    if (!response.ok) {
-      throw new Error("Failed to update blog post");
+    try {
+      const response = await fetch(
+        `https://v2.api.noroff.dev/blog/posts/andre_lier/${postId}`,
+        {
+          method: 'PUT',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiYW5kcmVfbGllciIsImVtYWlsIjoiYW5kbGllMDIxNzRAc3R1ZC5ub3JvZmYubm8iLCJpYXQiOjE3MzEwMDQyNjd9.VTkDBUpQPgFpauw6Qvp-KFiFUplT3Q71tFm9nHn2Ce0`,
+            'X-Noroff-API-Key': '0ce68424-2b01-48d9-871d-b9e4c13166d2',
+          },
+          body: JSON.stringify(updatedContent),
+        }
+      );
+
+      if (!response.ok) {
+        throw Error("Failed to update blog post");
+      }
+      alert("Posten ble oppdatert");
+      window.location.href="edit.html";
+    } catch (error) {
+      console.error("Failed to update blog post:", error);
+      alert('An error occurred while updating the post: ' + error.message);
     }
-
-    alert("Post updated successfully!");
-    window.location.reload(); 
-  } catch (error) {
-    console.error("Failed to update blog post:", error);
-    alert("An error occurred while updating the post: " + error.message);
-  }
-});
-
+  });
 
 fetchPostData();
--- code/js_files/Direct/index.js
+++ code/js_files/Direct/index.js
@@ -1,35 +1,120 @@
 const token = localStorage.getItem("token");
 
-import { allBlogPostfetch } from "../api_calls/api_fetch.js";
+import {
+  allBlogPostfetch,
+  filterbloggpostFetch,
+} from "../api_calls/api_fetch.js";
 
-async function displayBlogPosts() {
-    try {
-        const posts = await allBlogPostfetch();
-        
-        const blogContainer = document.getElementById("data-container");
-        blogContainer.innerHTML = ""; 
+async function displayOstlandetBlogPosts() {
+  try {
+    const posts = await allBlogPostfetch();
 
-        posts.data.forEach(post => {
-            const postElement = document.createElement("div");
-            postElement.classList.add("post");
+    const ostlandetContainer = document.getElementById("Ostlandet-container");
+    ostlandetContainer.innerHTML = "";
 
+    const postsToDisplay = posts.data.slice(0, 3);
 
-            postElement.innerHTML = `
+    postsToDisplay.forEach((post) => {
+      const postElement = document.createElement("div");
+      postElement.classList.add("post");
+
+      postElement.innerHTML = `
                 <img src="${post.media.url}" alt="${post.media.alt}">
                 <h3>${post.title}</h3>
-                <p>${post.body.split('.')[0]}</p>
+                <p>${post.body.split(".")[0]}</p>
                 <p><small>Author: ${post.author.name}</small></p>
             `;
 
-            postElement.addEventListener('click', () => {
-                window.location.href = `blogpost.html?id=${post.id}`; 
-            });
+      postElement.addEventListener("click", () => {
+        window.location.href = `blogpost.html?id=${post.id}`;
+      });
 
-            blogContainer.appendChild(postElement);
-        });
-    } catch (error) {
-        console.error("Det oppstod en feil ved henting av blogginnlegg:", error);
-    }
+      ostlandetContainer.appendChild(postElement);
+    });
+  } catch (error) {
+    console.error(
+      "Det oppstod en feil ved henting av blogginnlegg for Ostlandet:",
+      error
+    );
+  }
 }
 
+async function displayBlogPosts() {
+  try {
+    const posts = await allBlogPostfetch();
+
+    const blogContainer = document.getElementById("data-container");
+    blogContainer.innerHTML = "";
+
+    posts.data.forEach((post) => {
+      const postElement = document.createElement("div");
+      postElement.classList.add("post");
+
+      postElement.innerHTML = `
+                <img src="${post.media.url}" alt="${post.media.alt}">
+                <h3>${post.title}</h3>
+                <p>${post.body.split(".")[0]}</p>
+                <p><small>Author: ${post.author.name}</small></p>
+            `;
+
+      postElement.addEventListener("click", () => {
+        window.location.href = `blogpost.html?id=${post.id}`;
+      });
+
+      blogContainer.appendChild(postElement);
+    });
+  } catch (error) {
+    console.error("Det oppstod en feil ved henting av blogginnlegg:", error);
+  }
+}
+
+async function displayByFilters(selectregion) {
+  try {
+    const posts = await filterbloggpostFetch(selectregion);
+const ostlandetContainer = document.getElementById("data-container");
+ostlandetContainer.innerHTML = "";
+    posts.data.forEach((post) => {
+        const postElement = document.createElement("div");
+        postElement.classList.add("post");
+  
+        postElement.innerHTML = `
+          <img src="${post.media.url}" alt="${post.media.alt}">
+          <h3>${post.title}</h3>
+          <p>${post.body.split(".")[0]}</p>  <!-- Kun første setning -->
+          <p><small>Author: ${post.author.name}</small></p>
+        `;
+  
+
+        postElement.addEventListener("click", () => {
+          window.location.href = `blogpost.html?id=${post.id}`;  
+        });
+
+        ostlandetContainer.appendChild(postElement);
+    });
+  } catch (error) {
+    console.error(
+      "Det oppstod en feil ved henting av blogginnlegg for Ostlandet:",
+      error
+    );
+  }
+}
+
+const filter = document.querySelector(".filterByPlace");
+if (filter) {
+  filter.addEventListener("change", () => {
+    const selectregion = filter.value;
+    if (selectregion !== "all") {
+      displayByFilters(selectregion);
+    } else {
+      displayBlogPosts();
+    }
+  });
+}
+
+function popuptext() {
+    document.getElementById("stifinnetenmaskottext");
+    display = "block";
+  }  
+
+displayOstlandetBlogPosts();
 displayBlogPosts();
