98bb3f7d96fed628249b81b60f88e08b8d284b22
Andlie94 <160486912+Andlie94@users.noreply.github.com>
2024-11-21 11:36:29 +0100

    Delete code directory

--- code/js_files/Direct/checklogin.js
+++
@@ -1,8 +0,0 @@
-function isLoggedIn() {
-    return localStorage.getItem("token") !== null; 
-}
-if (isLoggedIn()) {
-    console.log("Brukeren er innlogget.");
-} else {
-    console.log("Brukeren er ikke innlogget.");
-}
